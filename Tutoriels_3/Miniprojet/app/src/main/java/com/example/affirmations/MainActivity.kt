a88aae411f71e8852c2b3044803669f6236e7aea
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2024-11-30 12:10:03 +0100

    Ajouter miniProjet 3

--- Tutoriels_3/Miniprojet/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Tutoriels_3/Miniprojet/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,12 +20,21 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.affirmations.ui.theme.AffirmationsTheme
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.toMutableStateList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,21 +52,28 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-val affirmations = listOf(
+val affirmations = mutableListOf(
     "Crois en toi-même et en toutes tes capacités.",
     "Chaque jour est une nouvelle opportunité.",
+    "Tu es plus fort que tu ne le penses.",
+    "N'abandonne jamais, les miracles prennent du temps.",
+    "Le succès commence par la volonté de l'atteindre.",
     "Tu es plus fort que tu ne le penses.",
     "N'abandonne jamais, les miracles prennent du temps.",
     "Le succès commence par la volonté de l'atteindre."
 )
 @Composable
 fun AffirmationCard(affirmation: String,modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+    val color =
+                if (expanded) MaterialTheme.colorScheme.surface
+                else MaterialTheme.colorScheme.onPrimary
 
     Button(
-        onClick = { /*TODO*/ },
+        onClick = { expanded = !expanded },
         //modifier = Modifier.background(null)
         colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Transparent
+        containerColor = Color.Transparent
         )
         ) {
         Card(
@@ -61,7 +81,8 @@ fun AffirmationCard(affirmation: String,modifier: Modifier = Modifier) {
                 .fillMaxWidth()
                 .padding(vertical = 8.dp),
             elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
-            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface,
+            colors = CardDefaults.cardColors(containerColor = color,
+
             )
         ) {
             Text(text = affirmation,
@@ -70,19 +91,66 @@ fun AffirmationCard(affirmation: String,modifier: Modifier = Modifier) {
                 color = MaterialTheme.colorScheme.onSurface
             )
         }
-    }
+       }
 
 }
+
+
+
+
+
 @Composable
 fun AffirmationList(affirmations: List<String>) {
+    var affirmationtext by remember { mutableStateOf("") }
+    val affirmationsState = remember {affirmations.toMutableStateList()}
+    val affirmationsList  : List<String> = affirmationsState.toList()
+
     LazyColumn(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        items(affirmations) { affirmation ->
-            AffirmationCard(affirmation)
+            modifier = Modifier.padding(16.dp)
+        ) {
+            item { AddAffirmationfield(affirmationtext,{affirmationtext = it})
+                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                    Button(
+                        onClick = {
+                            affirmationsState.add(affirmationtext)
+                            affirmationtext = ""
+                                  } ,
+                    )
+                    {
+                        Text(text = "Ajouter")
+                    }
+                }
+             }
+            items(affirmationsList) { it ->
+                AffirmationCard(it)
+            }
+
         }
-    }
+
+
 }
+
+@Composable
+fun AddAffirmationfield(affirmation: String,onValueChanged: (String) -> Unit,modifier: Modifier =Modifier)
+{
+
+    TextField(
+        value = affirmation,
+        onValueChange = onValueChanged,
+        singleLine = true,
+        placeholder = { Text("Ajouter une affirmation") },
+        label = { Text("Affirmation") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+    )
+//    Button { affirmations.add("test") }
+
+
+}
+
+
+
 
 @Preview()
 @Composable
